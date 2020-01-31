52d612c8be36ba8e942d4eb732d09c0d3021d011
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-01-31 16:30:57 +0300

    перекомпановка элементов
    Некоторые классы убраны в другие файлы...

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # важные импорты
 import pygame
-from CONSTANTS import width, height, fps
+from CONSTANTS import width, height
+
 
 # установка окружния основные иниты
 clock = pygame.time.Clock()
@@ -9,33 +10,12 @@ running = True
 screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
 pygame.display.set_caption('Time Limit!')
 full_screen_mod = True
-
-# группы спрайтов
-menu_sprite = pygame.sprite.Group()
-player_sprite = pygame.sprite.Group()
-static_obj_sprite = pygame.sprite.Group()
-dinamic_obj_sprite = pygame.sprite.Group()
 fps = clock.tick() / 1000
-# Побочные импорты
-from Player import Player, Menu
-from map import Map
-
-class MenuSprite():
-    def __init__(self, group):
-        self.player = player_main
-        self.menu = Menu(group, self.player, screen)
-
-    def update(self, clock, fps):
-        self.menu.update(clock, fps)
+from Level import GameLevel
 
 
-# Переменные, создание объектов
-map_ = Map(screen)
-player_main = Player(player_sprite, map_)
-player_main.teleportTo(map_.first_player_coords[0], map_.first_player_coords[1])
-map_.changeCoords([-player_main.x_y[0] + width // 2,
-                   -player_main.x_y[1] + height // 2 + player_main.image.get_height() // 2 - 10])
-MenuSprite(menu_sprite)
+game = GameLevel(screen)
+
 # Основной цикл
 while running:
     for event in pygame.event.get():
@@ -51,23 +31,16 @@ while running:
                     screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
             # вспомогательные события
             if event.key == pygame.K_SPACE:
-                player_main.changeMadnessAbout(15)
+                game.player_main.changeMadnessAbout(15)
             if event.key == pygame.K_x:
-                player_main.changeHungerAbout(-15, fps)
+                game.player_main.changeHungerAbout(-15, fps)
             if event.key == pygame.K_g:
-                if player_main.i_may_go:
-                    player_main.i_may_go = False
+                if game.player_main.i_may_go:
+                    game.player_main.i_may_go = False
                 else:
-                    player_main.i_may_go = True
-    player_sprite.update(fps)
-
-    screen.fill((50, 50, 50))
-    map_.draw()
-
-    player_sprite.draw(screen)
-    menu_sprite.update(fps)
-    menu_sprite.draw(screen)
-
+                    game.player_main.i_may_go = True
 
     fps = clock.tick() / 1000
+    game.update(fps)
+    game.render(fps)
     pygame.display.flip()
