255f4e1db89bf435c5333d12d048a41ffdc08e6a
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-01-31 16:36:40 +0300

    Перенос функции загрузки
    ВОТ СЮДА перенесена функция загрузки изображения...

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,8 +1,21 @@
 import pygame
+import os
+
+# функция для загрузки картинок
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 
 pygame.init()
 size = width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
-fps = 60
 
 # досаём карты из файла
 Maps = open('maps.txt', 'r')
@@ -14,3 +27,6 @@ for i in m:
         Maps.append([])
         continue
     Maps[-1].append(i[:-1])
+
+# статические объекты, доступные для создания
+static_obj_param = {'test_obj': {'num_of_anim': 0, 'images': ['img_l', 'img_r', 'img_u', 'img_d'], 'comment': ''}}
