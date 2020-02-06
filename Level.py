d0c6320b4f981b5300d1879621876a7a97b47e05
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-02-06 20:19:59 +0300

    Добавения и окончания игры
    +Добавлены окончание игры - можно выиграть и проиграть
    + добавлен метод очистки уровня.
    + добавлены картинки в меню

--- Level.py
+++ Level.py
@@ -2,23 +2,26 @@ import pygame
 from CONSTANTS import width, height, load_image, font
 from map import Map
 from Player import Player, Menu
-from BaseObjectClasses import StaticObject
+from BaseObjectClasses import StaticObject, InteractionContainer, InteractionObject
 
 
 class GameLevel():
-    def __init__(self, screen):
+    def __init__(self, screen, main_menu):
         self.screen = screen
+        self.main_menu = main_menu
         # группы спрайтов
         self.menu_sprite = pygame.sprite.Group()
         self.player_sprite = pygame.sprite.Group()
         self.static_obj_not_in_frame = []
         self.dinamic_obj_not_in_frame = []
+        self.interaction_obj_not_in_frame = []
+        self.interaction_obj_sprite = pygame.sprite.Group()
         self.static_obj_sprite = pygame.sprite.Group()
         self.dinamic_obj_sprite = pygame.sprite.Group()
 
         # объекты
         self.map_ = Map(self.screen)  # карта
-        self.player_main = Player(self.player_sprite, self.map_)  # игрок
+        self.player_main = Player(self.player_sprite, self.map_, self)  # игрок
         self.player_main.teleportTo(self.map_.first_player_coords[0], self.map_.first_player_coords[1])
 
         self.map_.changeCoords([-self.player_main.x_y[0] + width // 2,
@@ -26,29 +29,66 @@ class GameLevel():
 
         self.menu = InGameMenuSprite(self.menu_sprite, self.player_main, self.screen)
         self.levelGenerator()
+        self.level_state = 'playing'
 
     def getCoordsOnMap(self, coords):
         coords_on_map = (
             coords[0] - self.map_.coords_about_screean[0], coords[1] - self.map_.coords_about_screean[1])
         return coords_on_map
 
-    def render(self, fps, point):
+    def cleanLevel(self):
+        self.menu_sprite = pygame.sprite.Group()
+        self.player_sprite = pygame.sprite.Group()
+        self.static_obj_not_in_frame = []
+        self.dinamic_obj_not_in_frame = []
+        self.interaction_obj_not_in_frame = []
+        self.interaction_obj_sprite = pygame.sprite.Group()
+        self.static_obj_sprite = pygame.sprite.Group()
+        self.dinamic_obj_sprite = pygame.sprite.Group()
+
+        # объекты
+        self.map_ = Map(self.screen)  # карта
+        self.player_main = Player(self.player_sprite, self.map_, self)  # игрок
+        self.player_main.teleportTo(self.map_.first_player_coords[0], self.map_.first_player_coords[1])
+
+        self.map_.changeCoords([-self.player_main.x_y[0] + width // 2,
+                                -self.player_main.x_y[1] + height // 2 + self.player_main.image.get_height() // 2 - 10])
+
+        self.menu = InGameMenuSprite(self.menu_sprite, self.player_main, self.screen)
+        self.levelGenerator()
+        self.level_state = 'playing'
+
+    def render(self, fps, point, mouse_button_down, mouse_button_down_r):
         self.screen.fill((50, 50, 50))
         self.map_.draw()
         self.static_obj_sprite.update(fps, point)
-        self.player_sprite.draw(self.screen)
         self.static_obj_sprite.draw(self.screen)
+        self.interaction_obj_sprite.draw(self.screen)
+        self.interaction_obj_sprite.update(fps, point, mouse_button_down, mouse_button_down_r)
+        self.player_sprite.draw(self.screen)
         self.menu_sprite.update(fps, point)
         self.menu_sprite.draw(self.screen)
+        if self.level_state == 'win':
+            self.main_menu.game_stat = 'WinFrame'
+            self.cleanLevel()
+            return 0
+        if self.level_state == 'die':
+            self.main_menu.game_stat = 'DieFrame'
+            self.cleanLevel()
+            return 0
 
-    def update(self, fps, point, mouse_button_down):
+    def update(self, fps, point, mouse_button_down, mouse_button_down_r):
+
         self.player_sprite.update(fps, self.getCoordsOnMap(point), mouse_button_down)
 
     def levelGenerator(self):
         self.static_obj_not_in_frame.append(
             StaticObject(self.static_obj_sprite, self.screen, [3 * 300 + 50, 1 * 300 - 150], self.map_,
                          'broken_portal'))
-
+        self.interaction_obj_not_in_frame.append(InteractionObject(self.interaction_obj_sprite, self.screen,
+                                                                   [24 * 300 + 50, 3 * 300 - 150], self.map_, 'cup_of_power', self.player_main))
+        self.interaction_obj_not_in_frame.append(InteractionObject(self.interaction_obj_sprite, self.screen,
+                                                                   [24 * 300 + 50, 11 * 300 - 150], self.map_, 'end_portal', self.player_main))
 
 class InGameMenuSprite():
     def __init__(self, group, player, screen):
@@ -60,7 +100,14 @@ class InGameMenuSprite():
 
 
 class MainMenu():
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, img=None, oposit=True, exit_lambds=lambda met: exit(0)):
+        self.image = None
+        if not img is None:
+            self.image = pygame.sprite.Group()
+            self.sprite = pygame.sprite.Sprite(self.image)
+            self.sprite.image = load_image(img)
+            self.sprite.rect = self.sprite.image.get_rect()
+            self.sprite.rect.x, self.sprite.rect.y = 0, 0
         self.screen = screen
         self.game_stat = 'Main_menu'
         # группы спрайтов
@@ -70,11 +117,12 @@ class MainMenu():
         self.b_list.append(
             Button(self.buttons, 'interface_images/main_menu/play_b.png', x, y, lambda met: 'Playing', self))
         y = y + 50 + 390 // 2
+        if oposit:
+            self.b_list.append(
+                Button(self.buttons, 'interface_images/main_menu/options_b.png', x, y, lambda met: 'Pause_menu', self))
+            y = y + 50 + 390 // 2
         self.b_list.append(
-            Button(self.buttons, 'interface_images/main_menu/options_b.png', x, y, lambda met: 'Pause_menu', self))
-        y = y + 50 + 390 // 2
-        self.b_list.append(
-            Button(self.buttons, 'interface_images/main_menu/exit_b.png', x, y, lambda met: exit(0), self))
+            Button(self.buttons, 'interface_images/main_menu/exit_b.png', x, y, exit_lambds, self))
 
     def update(self, point, mouse_button_down):
         self.buttons.update(point, mouse_button_down)
@@ -84,6 +132,8 @@ class MainMenu():
 
     def render(self):
         self.screen.fill((230, 230, 230))
+        if not self.image is None:
+            self.image.draw(self.screen)
         self.buttons.draw(self.screen)
 
 
@@ -123,13 +173,10 @@ class Button(pygame.sprite.Sprite):
 
 
 class PauseMenu(MainMenu):
-    def __init__(self, screen, main_menu, x, y):
-        super().__init__(screen, x, y)
+    def __init__(self, screen, main_menu, x, y, img=None):
+        super().__init__(screen, x, y, exit_lambds=lambda u: 'Main_menu', oposit=False, img=img)
         self.main_manu = main_menu
 
     def pressMouseButton(self, state):
         self.main_manu.pressMouseButton(state)
 
-    def render(self):
-        self.screen.fill((0, 0, 0))
-        self.buttons.draw(self.screen)
