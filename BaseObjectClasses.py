6f3cabadce109006261beccb5be6a044c135693b
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-02-12 00:40:33 +0300

    Предметы и инвентарь. исправления.
    + Добавлены предметы(базовый класс, будет использоваться для материалов без возможности другого использования)
    + добавлен класс инвентаря, пока что используется только для того чтобы создать инвентарь игрока, позже от него нужно будет унаследовать класс, который будет использоваться для инвентаря контейнеров(интерактивных объектов с возможностью оставлять там предметы)
    + исправлен вывод картинок базового класса объектов.

--- BaseObjectClasses.py
+++ BaseObjectClasses.py
@@ -1,5 +1,16 @@
 import pygame
-from CONSTANTS import height, width, static_obj_param, load_image, font, thing_param, interaction_obj_param
+from CONSTANTS import height, width, static_obj_param, load_image, font, thing_param, interaction_obj_param, mod, font1
+
+
+def burh(self):
+    DefaultThing(self.player.level.things_ogf_spr, 'stick_matirial', [self.x_y[0], self.x_y[1] + 50], 2,
+                 self.player.level)
+    self.may_be_interacted = False
+
+interaction_obj_param['bush']={'num_of_anim': 1,
+                              'images': ['interaction_object_image/stick_bush.png', 'img_r', 'img_u', 'img_d'],
+                              'image_w': 200, 'image_h': 137, 'rows': 1, 'colomns': 2,
+                              'comment': 'a bush. Ok.', 'fuc': burh}
 
 
 class BaseObject(pygame.sprite.Sprite):
@@ -42,22 +53,25 @@ class StaticObject(BaseObject):
         self.param = static_obj_param[obj_name]
         self.num_of_anim = self.param['num_of_anim']
         self.images = [load_image(i) for i in self.param['images'][:1]]
-
-        for i in range(0, (self.num_of_anim + 1) * self.param['image_w'], self.param['image_w'] * self.param['rows']):
-            self.cut_sheet(self.images[0], self.param['colomns'], self.param['rows'], i, 0)
-
-        self.image = self.frames[self.naptr][self.cur_frame[0]][self.cur_frame[1]]
+        if self.num_of_anim >= 1:
+            for i in range(0, (self.num_of_anim + 1) * self.param['image_w'],
+                           self.param['image_w'] * self.param['rows']):
+                self.cut_sheet(self.images[0], self.param['colomns'], self.param['rows'], i, 0)
+            self.image = self.frames[self.naptr][self.cur_frame[0]][self.cur_frame[1]]
+        else:
+            self.image = self.images[0]
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.getCoordsInFrame()
 
     def update(self, fps, point):
-        self.i_j_k += 1
         self.rect.x, self.rect.y = self.getCoordsInFrame()
         self.collideMouse(point)
-        if self.i_j_k % self.frames_in_itter == 0:
-            self.cur_frame = (
-                self.cur_frame[0], (self.cur_frame[1] + 1) % len(self.frames[self.naptr][self.cur_frame[0]]))
-            self.image = self.frames[self.naptr][self.cur_frame[0]][self.cur_frame[1]]
+        if self.num_of_anim>=1:
+            self.i_j_k += 1
+            if self.i_j_k % self.frames_in_itter == 0:
+                self.cur_frame = (
+                    self.cur_frame[0], (self.cur_frame[1] + 1) % len(self.frames[self.naptr][self.cur_frame[0]]))
+                self.image = self.frames[self.naptr][self.cur_frame[0]][self.cur_frame[1]]
 
     def collideMouse(self, point):
         if self.rect.collidepoint(point):
@@ -75,6 +89,7 @@ class InteractionObject(BaseObject):
     def __init__(self, group, screen, x_y, map_, obj_name, player):
         self.screen = screen
         self.player = player
+        self.may_be_interacted = True
         super().__init__(group, x_y, map_)
         self.param = interaction_obj_param[obj_name]
         self.interactionAction = self.param['fuc']
@@ -110,10 +125,10 @@ class InteractionObject(BaseObject):
         pass
 
     def interaction(self):
-        if not self.interacted and self.player.collideRectImMap(self.rect):
+        if not self.interacted and self.player.collideRectImMap(self.rect) and self.may_be_interacted:
             self.interacted = True
             self.interactionAction(self)
-        elif self.interacted and self.player.collideRectImMap(self.rect):
+        elif self.interacted and self.player.collideRectImMap(self.rect) and self.may_be_interacted:
             self.interacted = False
 
     def collideMouse(self, point, mouse_button_down, mouse_button_down_r):
@@ -131,48 +146,137 @@ class InteractionContainer(InteractionObject):
                                                     , self.rect.y])
 
 class DefaultThing(pygame.sprite.Sprite):
-    def __init__(self, group, obj_name, x_y, stack_size):
+    def __init__(self, group, obj_name, x_y, stack_size, level):
         super().__init__(group)
+        self.name = obj_name
+        self.level = level
         self.param = thing_param[obj_name]
-        self.image = self.param['image']
+        self.image = load_image(self.param['image'])
         self.rect = self.image.get_rect()
-        self.rect.x, self.rect.y = x_y
-        self.stack_size = stack_size
+        if self.name != 'default_thing':
+            self.x_y = x_y
+            self.rect.x, self.rect.y = self.getCoordsInFrame()
+            self.stack_size = stack_size
+            self.taked = False
+            self.eq_coords = (0, 0)
+            self.in_box = False
+        else:
+            self.rect.x, self.rect.y = x_y
+        self.box = None
+
+    def getCoordsInFrame(self):
+        coords_on_screen = (
+            self.level.map_.coords_about_screean[0] + self.x_y[0], self.level.map_.coords_about_screean[1] + self.x_y[1])
+        return coords_on_screen
+
+    def setBox(self,box, x, y):
+        self.in_box = True
+        self.box = box
         self.taked = False
+        self.rect.x, self.rect.y = x, y
+
+    def getCoordsOnMap(self, coords):
+        coords_on_map = (
+            coords[0] - self.level.map_.coords_about_screean[0], coords[1] - self.level.map_.coords_about_screean[1])
+        return coords_on_map
 
     def update(self, fps, point, mouse):
-        if mouse and self.rect.collidepoint(point):
-            self.taked = True
-        if self.taked:
-            self.rect.x, self.rect.y = point[0], point[1]
+        collide_or_not = self.rect.collidepoint(point)
+        if self.name != 'default_thing':
+            if collide_or_not:
+                self.showStack()
+            if mouse and collide_or_not and not self.taked:
+                self.taked = True
+                if self.in_box:
+                    self.in_box = False
+                    self.box.giveThing(self)
+                    self.box = None
+                self.eq_coords = (point[0] - self.rect.x, point[1] - self.rect.y)
+                return 0
+            if not self.in_box:
+                if self.taked:
+                    self.rect.x, self.rect.y = point[0] - self.eq_coords[0], point[1] - self.eq_coords[1]
+                if mouse and self.taked:
+                    self.taked = False
+                    self.x_y = self.getCoordsOnMap((self.rect.x, self.rect.y))
+                    self.rect.x, self.rect.y = self.getCoordsInFrame()
+                if not self.taked:
+                    self.rect.x, self.rect.y = self.getCoordsInFrame()
+            else:
+                if collide_or_not:
+                    text = font.render(self.param['actions'], 1, (83, 150, 104))
+                    text_x = self.rect.x + (self.rect.w - text.get_width()) // 2
+                    text_y = self.rect.y - 50
+                    self.level.screen.blit(text, (text_x, text_y))
 
 
-
-
-    def collideMouse(self, point):
-        if self.rect.collidepoint(point):
-            text = font.render(self.param['comment'], 1, (83, 189, 104))
-            text_x = self.rect.x + (self.rect.w - text.get_width()) // 2
-            text_y = self.rect.y - 50
-            self.screen.blit(text, (text_x, text_y))
-
+    def showStack(self):
+        text = font1.render(str(self.stack_size), 1, (83, 150, 104))
+        text_x = self.rect.x + (self.rect.w - text.get_width()) // 2
+        text_y = self.rect.y + self.rect.h + 3
+        self.level.screen.blit(text, (text_x, text_y))
+        temp = text.get_height()
+        text = font1.render(' '.join(self.name.split('_')).title(), 1, (83, 150, 104))
+        text_x = self.rect.x + (self.rect.w - text.get_width()) // 2
+        text_y = self.rect.y + self.rect.h + 6 + temp
+        self.level.screen.blit(text, (text_x, text_y))
 
 
 class ThingBox(pygame.sprite.Sprite):
-    def __init__(self, gruop, num_of_cell, x_y):
+    def __init__(self, gruop, num_of_cell, x_y, player):
         super().__init__(gruop)
-        self.image = pygame.Surface([100, num_of_cell * 70 + (num_of_cell + 1) * 10])
+        self.player = player
+        self.image = pygame.Surface([num_of_cell * 70 + (num_of_cell + 1) * 10, 90])
         self.image.fill(pygame.Color("brown"))
         self.rect = self.image.get_rect()
-        self.rect.x, self.recty = x_y
+        self.rect.x, self.rect.y = x_y
         self.cells_group = pygame.sprite.Group()
-        self.cells = [DefaultThing(self.cells_group, 'default_thing', [i, x_y[1]], 1) for i in range(10, num_of_cell * 80, 80)]
+        self.cells = [DefaultThing(self.cells_group, 'default_thing', [i, x_y[1] + 10], 1,
+                                   self.player.level) for i in range(self.rect.x + 10, self.rect.x + num_of_cell * 80, 80)]
+
+    def collideRectImMap(self, rect):
+        x, y, w, h = rect
+
+        x1, y1, w1, h1 = self.rect
+        if ((x <= x1 <= x + w and y <= y1 <= y + h) or
+                (x <= x1 + w1 <= x + w and y <= y1 + h1 <= y + h) or
+                (x <= x1 + w1 <= x + w and y <= y1 <= y + h) or
+                (x <= x1 <= x + w and y <= y1 + h1 <= y + h) or
+
+                (x1 <= x <= x1 + w1 and y1 <= y <= y1 + h1) or
+                (x1 <= x + w <= x1 + w1 and y1 <= y + h <= y1 + h1) or
+                (x1 <= x + w <= x1 + w1 and y1 <= y <= y1 + h1) or
+                (x1 <= x <= x1 + w1 and y1 <= y + h <= y1 + h1)):
+            return True
+        else:
+            return False
+
+    def collideAnyInMap(self):
+        for i in self.player.level.things_ogf_spr:
+            if self.collideRectImMap(i.rect) and i not in self.cells and not i.taked:
+                return i
 
     def takeThing(self, thing):
-        pass
+        indx = mod(- self.rect.x + thing.rect.x - 10) // 80
+        temp = self.cells[indx]
+        if temp.name != 'default_thing':
+            self.giveThing(self.cells[indx])
+        self.cells[indx].kill()
+        self.cells[indx] = thing
 
-    def giveThing(self, th_index):
-        pass
+        thing.setBox(self, self.rect.x + 10 + indx * 80, self.rect.y + 10)
+        self.cells_group.add(thing)
+
+
+    def giveThing(self, th):
+        indx = self.cells.index(th)
+        self.cells_group.remove(self.cells[indx])
+        self.cells[indx] = DefaultThing(self.cells_group, 'default_thing', [self.rect.x + indx * 80 + 10, self.rect.y + 10], 1,
+                                   self.player.level)
+
 
     def update(self):
-        pass
+        self.cells_group.draw(self.player.level.screen)
+        thing = self.collideAnyInMap()
+        if thing:
+            self.takeThing(thing)
